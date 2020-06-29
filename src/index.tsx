cae14a89c6f13b9a37a2f51b9266d9c0f6131a16
lujinggirl <lujing_girl@163.com>
2020-06-29 16:58:03 +0800

    redux

--- src/index.tsx
+++ src/index.tsx
@@ -1,14 +1,21 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
+import './index.scss';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
+import { Provider } from 'react-redux';
+import configureStore from './redux/store/store';
+import { createBrowserHistory } from "history";
+// 创建store
+const store = configureStore(createBrowserHistory())
+
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+  <Provider store={store}>
+  <App />
+  </Provider>,
+  document.getElementById('root') as HTMLElement
+
 );
 
 // If you want your app to work offline and load faster, you can change
